73300fbda1391d3a13c93d9fc6d11f8fa6358bf3
Aryan Gupta <aryand.gupta789@gmail.com>
2026-09-05 13:13:34 +0530

    Basic CRUD API

--- app.py
+++ app.py
@@ -16,4 +16,22 @@ def create_user():
         return jsonify({"Error":f"User with ID {data['id']} already exists"}),400
     users.append(data)
     return jsonify({"Message":"User created","Data":data}), 201
+@app.route("/users/<int:id>",methods=["PUT"])
+def update_user(id):
+    data=request.json
+    for i in users:
+        if i["id"]==id:
+            try:
+                i["name"]=data["name"]
+                return jsonify({"Message":"User updated","Data":i}),200
+            except KeyError:
+                return jsonify({"Error":"Invalid data format"}),400
+    return jsonify({"Error":f"User with ID {id} not found"}),404
+@app.route("/users/<int:id>",methods=["DELETE"])
+def delete_user(id):
+    for i in users:
+        if i["id"]==id:
+            users.remove(i)
+            return jsonify({"Message":"User deleted"}),200
+    return jsonify({"Error":f"User with ID {id} not found"}),404
 app.run(debug=True)
