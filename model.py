1472125c29e6b6664ffda647261a1878d089a003
Aryan Gupta <aryand.gupta789@gmail.com>
2026-09-09 11:42:12 +0530

    Using model_validate() & model_dump()

--- model.py
+++ model.py
@@ -4,8 +4,14 @@ class User(BaseModel):
     name: str=Field(min_length=1, max_length=100)
 try:
     user1 = User(id=1, name="John Doe")
+    data={"id": 2, "name": "Bob Johnson"}
+    user2 = User.model_validate(data)#pydantic model
     print(user1.id)#Returns 1
     print(user1.name)#Returns "John Doe"
-    user2 = User(id=-1, name="")#ValidationError
+    print(user2.id)#Returns 2
+    print(user2.name)#Returns "Bob Johnson"
+    data2=user2.model_dump()
+    print(data2)#dict
+    user3 = User(id=-1, name="")#ValidationError
 except ValidationError as e:
     print(e)
