ee02f6f925fa1272d1e17a5e397decfa67b35f01
Aryan Gupta <aryand.gupta789@gmail.com>
2026-09-09 10:36:05 +0530

    Starting with BaseModel

--- model.py
+++ model.py
@@ -3,6 +3,6 @@ class User(BaseModel):
     id: int
     name: str
 user1 = User(id=1, name="John Doe")
-print(user1.id)
-print(user1.name)
-user2 = User(id=2)
+print(user1.id)#Returns 1
+print(user1.name)#Returns "John Doe"
+user2 = User(id=2)#ValidationError
